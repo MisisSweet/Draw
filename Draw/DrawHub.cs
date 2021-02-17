bc6b3a22206a699ec91992f5b0b203860cc716ba
MisisSweet <48993011+MisisSweet@users.noreply.github.com>
2021-02-18 01:01:03 +0300

    Добавьте файлы проекта.

---
+++ Draw/DrawHub.cs
@@ -0,0 +1,16 @@
+﻿using Draw.Models;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Draw
+{
+    public class DrawHub : Hub
+    {
+        public async Task Send(Data message)
+        {
+            await Clients.Others.SendAsync("Receive", message);
+        }
+    }
+}
---
+++ Draw/Models/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Draw.Models
+{
+    public class Data
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("operation")]
+        public int Operation { get; set; }
+        [JsonProperty("x")]
+        public float X { get; set; }
+        [JsonProperty("y")]
+        public float Y { get; set; }
+
+    }
+}
